a129aef7079f27317e83b42256e0a1bc9e63e46c
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2024-09-29 23:18:57 -0300

    FIX Atualizacao do servico

--- renda-fixa-service/Services/ProdutoRendaFixaService.cs
+++ renda-fixa-service/Services/ProdutoRendaFixaService.cs
@@ -7,12 +7,18 @@ public class ProdutoRendaFixaService : IProdutoRendaFixaService
 {
     private readonly ILogger<ProdutoRendaFixaService> log;
     private readonly IBaseRepository<ProdutoRendaFixa> repositorio;
+    private readonly IBaseRepository<Aporte> repositorioAporte;
+    private readonly IBaseRepository<Conta> repositorioConta;
     public ProdutoRendaFixaService(
         ILogger<ProdutoRendaFixaService> log,
-        IBaseRepository<ProdutoRendaFixa> repositorio)
+        IBaseRepository<ProdutoRendaFixa> repositorio,
+        IBaseRepository<Conta> repositorioConta,
+        IBaseRepository<Aporte> repositorioAporte)
     {
         this.log = log;
         this.repositorio = repositorio;
+        this.repositorioConta = repositorioConta;
+        this.repositorioAporte = repositorioAporte;
     }
 
     public async Task<IList<ProdutoRendaFixa>> GetAllAsync()
@@ -22,10 +28,35 @@ public class ProdutoRendaFixaService : IProdutoRendaFixaService
         return resultado;
     }
 
-    public async Task<ProdutoRendaFixa> GetByIdAsync(Guid id) 
+    public async Task<ProdutoRendaFixa> GetByIdAsync(int id) 
     {
         log.LogInformation($"Obtendo o produto de renda fixa com id {id}");
         var resultado = await repositorio.GetByIdAsync(id);
         return resultado;
+    }
+    //todo corrigir, adicionar o cancellation token
+    public async Task ComprarAsync(int produtoId, int contaId, int quantidade)
+    {
+        var conta = await repositorioConta.GetByIdAsync(contaId);
+        var produto = await repositorio.GetByIdAsync(produtoId);
+        if(quantidade < produto.Estoque)
+        {
+            var calculo = produto.PrecoUnitario * quantidade;
+            if(calculo < conta.Saldo)
+            {
+                conta.Saldo -= calculo;
+                await repositorioConta.UpdateAsync(conta);
+
+                var entidade = new Aporte(produtoId, contaId, DateTime.Now, 1);
+                await repositorioAporte.InsertAsync(entidade);
+
+                produto.Estoque -= quantidade;
+                await repositorio.UpdateAsync(produto);
+            }
+        }
+    }
+    public async Task<Conta> GetContaAsync(int contaId)
+    {
+        return await repositorioConta.GetByIdAsync(contaId);
     }
 }
