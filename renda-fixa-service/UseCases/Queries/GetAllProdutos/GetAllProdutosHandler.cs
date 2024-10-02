272a30d521463af38ae5d8cb2d31a3ca4833c6b0
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2024-10-02 20:51:50 -0300

    FIX Ajustes na requisição do get all produtos

---
+++ renda-fixa-service/UseCases/Queries/GetAllProdutos/GetAllProdutosHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using RendaFixa.Domain.Interfaces;
+
+namespace RendaFixa.Service.UseCases.Queries.GetAllProdutos;
+
+public class GetAllProdutosHandler : IRequestHandler<GetAllProdutosRequest, IList<GetAllProdutosResponse>>
+{
+    private readonly IProdutoRendaFixaRepository repository;
+    private readonly IMapper mapper;
+
+    public GetAllProdutosHandler(IProdutoRendaFixaRepository repository, IMapper mapper)
+    {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public async Task<IList<GetAllProdutosResponse>> Handle(GetAllProdutosRequest request, CancellationToken cancellationToken)
+    {
+        var resposta = await repository.GetProdutosAsync(cancellationToken);
+
+        return mapper.Map<IList<GetAllProdutosResponse>>(resposta);
+    }
+}
